cf154985a7aa2a2814fa71d111f23bd10f8d5785
inhwa1025 <inhwa9901@gmail.com>
2022-01-24 04:09:08 +0900

    baekjoon no.1041 - (2)

--- 1041_dice.py
+++ 1041_dice.py
@@ -12,15 +12,15 @@ if n == 1:
 three = two = sum(dice)
 
 for i in range(6):
-    cur_num = dice[5-i]
+    cur_sum = dice[5-i]
     tmp_dice = dice[0:i] + dice[i+1:6]
-    tmp_dice.remove(cur_num)
-    cur_num += min(tmp_dice)
-    if cur_num < two:
-        two = cur_num
+    tmp_dice.remove(cur_sum)
+    cur_sum += min(tmp_dice)
+    if cur_sum < two:
+        two = cur_sum
     tmp_dice.remove(min(tmp_dice))
-    cur_num += min(tmp_dice)
-    if cur_num < three:
-        three = cur_num
+    cur_sum += min(tmp_dice)
+    if cur_sum < three:
+        three = cur_sum
 
-print(three*4 + two*((n-2)*8+4) + min(dice)*((n-2)*(n-2)*5+(n-2)*4))
+print(three*4 + two*(8*n-12) + min(dice)*(5*n*n-16*n+12))
