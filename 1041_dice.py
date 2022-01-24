de9c28ace05298fcbb32b8d46032f734f67e8c26
inhwa1025 <inhwa9901@gmail.com>
2022-01-24 10:21:36 +0900

    baekjoon no.1041 - (3)

--- 1041_dice.py
+++ 1041_dice.py
@@ -9,18 +9,8 @@ if n == 1:
     print(sum(dice)-max(dice))
     exit(0)
 
-three = two = sum(dice)
+fcf = sorted([min(dice[0], dice[5]), min(dice[1], dice[4]), min(dice[2], dice[3])], reverse=True)
+two = fcf.pop() + fcf.pop()
+three =two + fcf.pop()
 
-for i in range(6):
-    cur_sum = dice[5-i]
-    tmp_dice = dice[0:i] + dice[i+1:6]
-    tmp_dice.remove(cur_sum)
-    cur_sum += min(tmp_dice)
-    if cur_sum < two:
-        two = cur_sum
-    tmp_dice.remove(min(tmp_dice))
-    cur_sum += min(tmp_dice)
-    if cur_sum < three:
-        three = cur_sum
-
-print(three*4 + two*(8*n-12) + min(dice)*(5*n*n-16*n+12))
+print(three*4 + two*(8*n-12) + min(dice)*((n-2)*(n-2)+(n-2)*(n-1)*4))
