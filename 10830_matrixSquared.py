987d4f479b0b086879d58ad8d414f00fa5a2c8c1
inhwa1025 <inhwa9901@gmail.com>
2022-01-28 10:42:03 +0900

    baekjoon no.10830 (timeout)

--- 10830_matrixSquared.py
+++ 10830_matrixSquared.py
@@ -4,22 +4,21 @@ size, b = map(int, sys.stdin.readline().split())
 matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(size)]
 
 
-def squared(p, before):
-    if p == 2:
-        after = [[0 for _ in range(size)] for _ in range(size)]
-        for i in range(size):
-            for j in range(size):
-                for k in range(size):
-                    after[i][j] += before[i][k] * matrix[k][j]
-                after[i][j] %= 1000
-        return after
-    else:
-        while p > 2:
-            after = squared(2, before)
-            return squared(p-1, after)
+def squared(before):
+    global matrix
+    after = [[0] * size for _ in range(size)]
+    for i in range(size):
+        for j in range(size):
+            for k in range(size):
+                after[i][j] += before[i][k] * matrix[k][j]
+            after[i][j] %= 1000
+    return after
 
 
-result = squared(b, matrix)
+result = matrix
+for _ in range(b-1):
+    result = squared(result)
+
 for m in range(size):
     for n in range(size):
         print(result[m][n], end=" ")
