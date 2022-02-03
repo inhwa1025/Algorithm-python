61596e259c74b98b82857345f7e1ebd57060af18
inhwa1025 <inhwa9901@gmail.com>
2022-02-03 10:30:05 +0900

    baekjoon no.10830 (timeout2)

--- 10830_matrixSquared.py
+++ 10830_matrixSquared.py
@@ -4,20 +4,27 @@ size, b = map(int, sys.stdin.readline().split())
 matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(size)]
 
 
-def squared(before):
-    global matrix
+def squared(matrix1, matrix2):
     after = [[0] * size for _ in range(size)]
     for i in range(size):
         for j in range(size):
             for k in range(size):
-                after[i][j] += before[i][k] * matrix[k][j]
+                after[i][j] += matrix1[i][k] * matrix2[k][j]
             after[i][j] %= 1000
     return after
 
 
-result = matrix
-for _ in range(b-1):
-    result = squared(result)
+def recursion(input_matrix, exponent):
+    global matrix
+    if exponent == 1:
+        return input_matrix
+    elif exponent%2 == 0:
+        return squared(recursion(input_matrix, exponent//2), recursion(input_matrix, exponent//2))
+    else:
+        return squared(recursion(input_matrix, exponent-1), matrix)
+
+
+result = recursion(matrix, b)
 
 for m in range(size):
     for n in range(size):
