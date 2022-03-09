1eb0fa70adad295f1cfeb3765a1acc36964589b6
inhwa1025 <inhwa9901@gmail.com>
2022-03-09 20:58:28 +0900

    baekjoon no.1926 - BFS, success

--- week7/1926_draw.py
+++ week7/1926_draw.py
@@ -1,37 +1,45 @@
+# 백준 1926번 그림
+# 그래프, 너비 우선 탐색(BFS), 깊이 우선 탐색(DFS)
 import sys
-sys.setrecursionlimit(10000000)
+from collections import deque
 
 height, width = map(int, sys.stdin.readline().split())
 draw = [list(map(int, sys.stdin.readline().split())) for _ in range(height)]
 
-visited = [[False for _ in range(height)] for _ in range(height)]
-size = []
+visited = [[False for _ in range(width)] for _ in range(height)]
+queue = deque()
+cnt = 0
+max_size = 0
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
-def dfs(x, y, idx):
+def bfs(x, y):
+    global max_size
+    queue.append((x, y))
     visited[x][y] = True
-    if y < width-1 and draw[x][y+1] == 1 and not visited[x][y+1]:
-        size[idx] += 1
-        dfs(x, y+1, idx)
-    if y > 1 and draw[x][y-1] and not visited[x][y-1]:
-        size[idx] += 1
-        dfs(x, y-1, idx)
-    if x < height-1 and draw[x+1][y] == 1 and not visited[x+1][y]:
-        size[idx] += 1
-        dfs(x+1, y, idx)
-    if x > 1 and draw[x-1][y] and not visited[x-1][y]:
-        size[idx] += 1
-        dfs(x-1, y, idx)
+    each_size = 1
+
+    while queue:
+        x, y = queue.popleft()
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if (0 <= nx < height) and (0 <= ny < width) and draw[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                queue.append((nx, ny))
+                each_size += 1
+
+    max_size = max(max_size, each_size)
 
 
 for i in range(height):
     for j in range(width):
         if draw[i][j] == 1 and not visited[i][j]:
-            size.append(1)
-            dfs(i, j, len(size)-1)
+            cnt += 1
+            bfs(i, j)
 
-print(len(size))
-if len(size):
-    print(max(size))
-else:
-    print(0)
+print(cnt)
+print(max_size)
