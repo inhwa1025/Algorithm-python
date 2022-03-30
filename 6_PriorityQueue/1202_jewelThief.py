fb8a7d3212a7b11f1eb548f6f706bfcf28214ad1
inhwa1025 <inhwa9901@gmail.com>
2022-03-30 18:37:24 +0900

    baekjoon no.1202   ❌

---
+++ 6_PriorityQueue/1202_jewelThief.py
@@ -0,0 +1,33 @@
+# 백준 1202번 보석 도둑
+# 자료 구조, 그리디 알고리즘, 정렬, 우선순위 큐
+import sys
+import heapq
+
+jewel = []
+bag = []
+result = 0
+
+n, k = map(int, sys.stdin.readline().split())
+
+for _ in range(n):
+    weight, value = map(int, sys.stdin.readline().split())
+    heapq.heappush(jewel, (-value, weight))
+
+for _ in range(k):
+    bag.append(int(sys.stdin.readline()))
+
+bag.sort()
+
+while jewel:
+    tmp = heapq.heappop(jewel)
+
+    for i in range(len(bag)):
+        if tmp[1] < bag[i]:
+            del bag[i]
+            result += -tmp[0]
+            break
+
+    if not bag:
+        break
+
+print(result)
