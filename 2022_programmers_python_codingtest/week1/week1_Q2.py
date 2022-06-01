9fda82e914d8916c376578d47445912d6f3a556a
inhwa1025 <inhwa9901@gmail.com>
2022-06-01 20:11:28 +0900

    programmers python codingtest class - week1 Q2

--- 2022_programmers_python_codingtest/week1/week1_Q2.py
+++ 2022_programmers_python_codingtest/week1/week1_Q2.py
@@ -1,19 +1,13 @@
 def solution(n, lost, reserve):
-    answer = n - len(lost)
-
     # 여벌 체육복을 가져온 학생이 도난을 당한 경우 먼저 처리
-    for lost_st in lost:
-        if lost_st in reserve:
-            answer += 1
-            reserve.remove(lost_st)
-            lost.remove(lost_st)
+    reserve_uniq = list(set(reserve) - set(lost))
+    lost_uniq = list(set(lost) - set(reserve))
 
     # 그 외
-    for lost_st in lost:
-        for res_st in reserve:
-            if lost_st - 1 <= res_st <= lost_st + 1:
-                answer += 1
-                reserve.remove(res_st)
-                break
+    for res_st in reserve_uniq:
+        if res_st - 1 in lost_uniq:
+            lost_uniq.remove(res_st - 1)
+        elif res_st + 1 in lost_uniq:
+            lost_uniq.remove(res_st + 1)
 
-    return answer
+    return n - len(lost_uniq)
