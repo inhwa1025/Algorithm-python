543b39a2271c9fb18a47b6dba5908a2f7de14ded
inhwa1025 <inhwa9901@gmail.com>
2022-06-03 19:17:52 +0900

    programmers python codingtest class - week1 Q4

--- 2022_programmers_python_codingtest/week1/week1_Q4.py
+++ 2022_programmers_python_codingtest/week1/week1_Q4.py
@@ -1,6 +1,19 @@
 from itertools import combinations
 
 
+# 채점 결과 정확성: 100.0/ 100.0
+def solution(number, k):
+    result = []
+
+    for n in number:
+        while k > 0 and result and result[-1] < n:
+            result.pop()
+            k -= 1
+        result.append(n)
+
+    return ''.join(result[:len(result) - k])
+
+
 # 채점 결과 정확성: 91.7/ 100.0
 def solution_2(number, k):
     arr = list(number)
