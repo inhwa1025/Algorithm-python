f4cdd43318780a46b7e015acc76abf1a3dfd78ff
inhwa1025 <inhwa9901@gmail.com>
2022-10-13 20:38:14 +0900

    2022 kakao blind

---
+++ CodingTest/2022_kakao_blind/Q5_1.py
@@ -0,0 +1,13 @@
+# 양과 늑대, DFS, 비트마스킹(중복 경로 체크)
+visited = [False for _ in range(pow(2, 17))]    # 비트마스킹
+
+
+def dfs(state):
+    if visited[state]:
+        return 0
+
+    pass
+
+
+def solution(info, edges):
+    return 0
